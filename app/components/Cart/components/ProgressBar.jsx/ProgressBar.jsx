a29bccdadb3b5e08fb9d8f98ba7cdc8d5832cf27
rohan-nordstone <rohan@nordstone.co.uk>
2024-03-28 20:14:08 +0530

    redux integration for cart

--- app/components/Cart/components/ProgressBar.jsx/ProgressBar.jsx
+++ app/components/Cart/components/ProgressBar.jsx/ProgressBar.jsx
@@ -5,8 +5,8 @@ const ProgressBar = ({ totalAmount }) => {
   // Define the thresholds and their corresponding icons
   const thresholds = [
     { amount: 0, label: '', icon: null }, // Starting threshold with no circle
-    { amount: 2000, label: 'Free Gift', icon: <FaGift /> },
-    { amount: 3000, label: 'Free Shipping', icon: <FaShippingFast /> },
+    { amount: 800, label: 'Free Gift', icon: <FaGift /> },
+    { amount: 1500, label: 'Free Shipping', icon: <FaShippingFast /> },
   ];
 
   // Calculate the percentage progress
@@ -26,17 +26,14 @@ const ProgressBar = ({ totalAmount }) => {
 
   return (
     <div className='my-8 relative px-4'>
-
       <div className="progress-bar bg-gray-200 w-full h-2 relative rounded-full overflow-hidden">
         <div className="progress bg-black h-full" style={{ width: `${progress}%` }} />
-
-        {/* Render circles for thresholds */}
       </div>
       <div className="thresholds absolute top-[-1rem] left-0 w-[90%] flex justify-between ">
         {thresholds.map((threshold, index) => (
-          <div key={index} className={`threshold text-center flex justify-center items-center flex-col w-auto relative ${totalAmount >= threshold.amount && index !== 0 ? 'text-black-500' : 'text-black-400'}`}>
+          <div key={index} className={`threshold text-center flex justify-center items-center flex-col w-auto relative ${totalAmount >= threshold.amount ? 'text-black-500' : 'text-black-400'}`}>
             {index !== 0 && (
-              <div className={`circle  rounded-full  w-10 h-10 flex items-center justify-center ${totalAmount >= threshold.amount ? 'bg-black text-white border border-black' : 'bg-white border border-black'}`}>
+              <div className={`circle rounded-full w-10 h-10 flex items-center justify-center ${totalAmount >= threshold.amount ? 'bg-black text-white border border-black' : 'bg-white border border-black'}`}>
                 {threshold.icon && <span className="text-lg">{threshold.icon}</span>}
               </div>
             )}
@@ -45,15 +42,11 @@ const ProgressBar = ({ totalAmount }) => {
         ))}
       </div>
       <div>
-
         {remaining > 0 && (
           <div className="text-sm w-full mt-12 mb-[-2rem] text-center">
-            <span className="font-bold text-lg">
-              {`Rs. ${remaining.toFixed(2)}`}
-            </span>
-            <span className="ml-1">more to reach {nextThreshold}</span> {/* Display next threshold label */}
+            <span className="font-bold text-lg">{`Rs. ${remaining.toFixed(2)}`}</span>
+            <span className="ml-1">more to reach {nextThreshold}</span> 
           </div>
-
         )}
       </div>
     </div>
